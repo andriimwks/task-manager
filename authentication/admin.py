a495f8e2b1ba6df3d747a5aa1c22f87ec7ab4610
Andrii <andrii.ctc@gmail.com>
2025-11-26 22:11:19 +0200

    feat(auth): Display users in admin

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    fieldsets = ()
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    list_filter = ('is_staff',)
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
