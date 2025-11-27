cd8bed6678865c332cd4a25e4b19113b95d557fb
Andrii <andrii.ctc@gmail.com>
2025-11-27 23:14:05 +0200

    feat: Create PartialProjects view to display project names via htmx

--- workspace/urls.py
+++ workspace/urls.py
@@ -3,6 +3,9 @@ from .views import *
 
 
 app_name = 'workspace'
+
+
 urlpatterns = [
-    path('', Dashboard.as_view(), name='dashboard')
+    path('', Dashboard.as_view(), name='dashboard'),
+    path('partial/projects', PartialProjects.as_view(), name='partial_projects'),
 ]
--- workspace/views.py
+++ workspace/views.py
@@ -1,6 +1,17 @@
+from django.shortcuts import render
+from django.views import View
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Project
 
 
 class Dashboard(TemplateView):
     template_name = 'workspace/dashboard.html'
+
+
+class PartialProjects(LoginRequiredMixin, View):
+    template_name = 'workspace/partials/projects.html'
+
+    def get(self, request):
+        projects = Project.objects.filter(created_by=request.user)
+        return render(request, self.template_name, {'projects': projects})
