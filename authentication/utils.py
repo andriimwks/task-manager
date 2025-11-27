91acf1e73f48d936f85841fce903737bf717c1bc
Andrii <andrii.ctc@gmail.com>
2025-11-27 22:00:33 +0200

    feat(auth): Add utility to mask email addresses

---
+++ authentication/utils.py
@@ -0,0 +1,7 @@
+def mask_email(email: str) -> str:
+    """Returns a string with a partially censored local part of given email address."""
+    local_part, domain = email.split('@')
+    masked_local = f"{local_part[0]}{'*' * (len(local_part) - 2)}{local_part[-1]}" if len(local_part) > 2 else '*' * len(local_part)
+    return "%s@%s" % (masked_local, domain)
+
+
