1cd810cfcb0e690b663960e99edf750ca16b5df9
Andrii <andrii.ctc@gmail.com>
2025-11-28 01:19:49 +0200

    docs(workspace): Add docstring to the Dashboard view

--- workspace/views/main.py
+++ workspace/views/main.py
@@ -3,4 +3,5 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class Dashboard(LoginRequiredMixin, TemplateView):
+    """An entry point to the workspace, which further acts as a SPA."""
     template_name = 'workspace/dashboard.html'
