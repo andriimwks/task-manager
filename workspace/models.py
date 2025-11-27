80b1e0a76909a5aa71abc59f601cc219e655860e
Andrii <andrii.ctc@gmail.com>
2025-11-27 18:02:34 +0200

    feat(workspace): Create Project model

--- workspace/models.py
+++ workspace/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    """Basic project model. Used primarily to separate task into \"groups\"."""
+
+    name = models.CharField('Name', max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
