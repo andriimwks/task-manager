f2ec92e1db283cf508d8a8fbed0ca4a1e420fa89
Andrii <andrii.ctc@gmail.com>
2025-11-27 18:10:06 +0200

    feat(workspace): Add 'created_by' field to Project model

--- workspace/models.py
+++ workspace/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
 
 
 class Project(models.Model):
     """Basic project model. Used primarily to separate task into \"groups\"."""
 
     name = models.CharField('Name', max_length=255)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
 
     def __str__(self) -> str:
         return self.name
