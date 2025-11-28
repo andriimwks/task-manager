8d41d228437c281a278dacd535d3153efec56c32
Andrii <andrii.ctc@gmail.com>
2025-11-28 12:19:12 +0200

    feat(workspace): Add base template for adding a project

--- workspace/urls.py
+++ workspace/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import main, partials
+from .views import main, projects, partials
 
 
 app_name = 'workspace'
@@ -7,5 +7,6 @@ app_name = 'workspace'
 
 urlpatterns = [
     path('', main.Dashboard.as_view(), name='dashboard'),
+    path('add-project/', projects.AddProject.as_view(), name='add_project'),
     path('partial/projects', partials.Projects.as_view(), name='partial_projects'),
 ]
