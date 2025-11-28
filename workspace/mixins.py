c71da6612fd1c719e240fb511341fc1858c7fc35
Andrii <andrii.ctc@gmail.com>
2025-11-29 01:06:50 +0200

    feat(workspaces): Add TaskRequiredMixin

--- workspace/mixins.py
+++ workspace/mixins.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseBadRequest
-from .models import Project
+from .models import Project, Task
 
 
 class HtmxRequiredMixin:
@@ -12,16 +12,36 @@ class HtmxRequiredMixin:
 
 
 class ProjectRequiredMixin:
+    """Ensures the request references a valid project owned by the user."""
+    
     def dispatch(self, request, *args, **kwargs):
         project_id = kwargs.get('project_id')
         if not project_id:
             return HttpResponseBadRequest("Project ID required")
 
         project = Project.objects.filter(pk=project_id, created_by=request.user).first()
-        if not project_id:
+        if not project:
             return HttpResponseBadRequest("Project not found")
 
         setattr(request, 'project', project)
         del kwargs['project_id']
+
+        return super().dispatch(request, *args, **kwargs)
+
+
+class TaskRequiredMixin:
+    """Ensures the request references a valid task belonging to the user's project."""
+
+    def dispatch(self, request, *args, **kwargs):
+        task_id = kwargs.get('task_id')
+        if not task_id:
+            return HttpResponseBadRequest("Task ID required")
+
+        task = Task.objects.filter(pk=task_id, project__created_by=request.user).first()
+        if not task:
+            return HttpResponseBadRequest("Task not found")
+
+        setattr(request, 'task', task)
+        del kwargs['task_id']
 
         return super().dispatch(request, *args, **kwargs)
