3955a01fd5c55868e2b913d23090090059557472
Andrii <andrii.ctc@gmail.com>
2025-11-28 12:41:39 +0200

    refactor(workspace): Remove htmx's push_url (it turned out to be unnecessary)

--- workspace/views/projects.py
+++ workspace/views/projects.py
@@ -2,12 +2,11 @@ from django.views import View
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
-from django_htmx.http import push_url
+# from django_htmx.http import push_url
 
 
 class AddProject(LoginRequiredMixin, View):
     template_name = 'workspace/add_project.html'
 
     def get(self, request):
-        response = render(request, self.template_name)
-        return push_url(response, reverse('workspace:add_project'))
+        return render(request, self.template_name)
