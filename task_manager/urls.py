90b6c6fb377fed3a6225ec1cfa1e3f59f24b3452
Andrii <andrii.ctc@gmail.com>
2025-11-29 21:21:26 +0200

    refactor(workspace): Handle index as workspace dashboard

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("allauth.urls")),
-    path("workspace/", include("workspace.urls")),
+    path("", include("workspace.urls")),
 ]
