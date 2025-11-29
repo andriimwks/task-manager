16f269e9fd209b56e705f95349d51975a4180606
Andrii <andrii.ctc@gmail.com>
2025-11-29 20:15:11 +0200

    refactor(workspace): Use ProjectRequiredMixin in partial TaskList view

--- workspace/views/partials.py
+++ workspace/views/partials.py
@@ -1,8 +1,7 @@
 from django.views import View
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponseNotFound
-from ..mixins import HtmxRequiredMixin
+from ..mixins import HtmxRequiredMixin, ProjectRequiredMixin
 
 
 class ProjectList(LoginRequiredMixin, HtmxRequiredMixin, View):
@@ -16,14 +15,12 @@ class ProjectList(LoginRequiredMixin, HtmxRequiredMixin, View):
         )
 
 
-class TaskList(LoginRequiredMixin, HtmxRequiredMixin, View):
+class TaskList(LoginRequiredMixin, HtmxRequiredMixin, ProjectRequiredMixin, View):
     """Renders a partial template containing the list of tasks belonging to the given project."""
 
     template_name = "workspace/partials/task_list.html"
 
-    def get(self, request, project_id: int):
-        project = request.user.projects.filter(pk=project_id).first()
-        if not project:
-            return HttpResponseNotFound("Project not found")
-
-        return render(request, self.template_name, {"tasks": project.tasks.all()})
+    def get(self, request):
+        return render(
+            request, self.template_name, {"tasks": request.project.tasks.all()}
+        )
