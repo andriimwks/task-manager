3c019c55da82d9eee7e316244501dc843587d690
Andrii <andrii.ctc@gmail.com>
2025-12-03 21:27:03 +0200

    feat(workspace): Add labels to form fields

--- workspace/forms.py
+++ workspace/forms.py
@@ -4,31 +4,39 @@ from django import forms
 class CreateProjectForm(forms.Form):
     """Form for creating a new project."""
 
-    project_name = forms.CharField(max_length=255, min_length=3, required=True)
+    project_name = forms.CharField(
+        label="Name", max_length=255, min_length=3, required=True
+    )
 
 
 class UpdateProjectForm(forms.Form):
     """Form for editing the name of an existing project."""
 
-    project_name = forms.CharField(max_length=255, min_length=3, required=True)
+    project_name = forms.CharField(
+        label="Name", max_length=255, min_length=3, required=True
+    )
 
 
 class CreateTaskForm(forms.Form):
     """Form for creating a new task."""
 
-    task_name = forms.CharField(max_length=255, min_length=3, required=True)
+    task_name = forms.CharField(
+        label="Name", max_length=255, min_length=3, required=True
+    )
 
 
 class UpdateTaskForm(forms.Form):
     """Form for editing an existing task."""
 
-    task_name = forms.CharField(max_length=255, min_length=3, required=True)
-    priority = forms.IntegerField(required=True)
-    deadline = forms.DateField(required=False)
-    completed = forms.BooleanField(required=False)
+    task_name = forms.CharField(
+        label="Name", max_length=255, min_length=3, required=True
+    )
+    priority = forms.IntegerField(label="Priority", required=True)
+    deadline = forms.DateField(label="Deadline", required=False)
+    completed = forms.BooleanField(label="Completed", required=False)
 
 
 class CompleteTaskForm(forms.Form):
     """Form for marking task is completed"""
 
-    completed = forms.BooleanField(required=False)
+    completed = forms.BooleanField(label="Completed", required=False)
